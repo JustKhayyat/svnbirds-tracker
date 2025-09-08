ef2113a4e95501cde6c55d5214b029ddf1929b2e
JustKhayyat <fadialkhayyat@outlook.com>
2025-09-09 00:47:35 +0800

    Fix Artists CSS import

--- pages/artist/[id].js
+++ pages/artist/[id].js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
-import styles from '../../styles/ArtistPage.module.css';
+import styles from '../../styles/Artists.module.css';
 
 export default function ArtistPage() {
   const router = useRouter();
